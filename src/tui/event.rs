51315188fad80fe0754c0347c39017dbdcae912a
Charles Dong <chardon_cs@proton.me>
2026-08-15 21:34:28 +0800

    docs: add rustdoc for `Event`

--- src/tui/event.rs
+++ src/tui/event.rs
@@ -1,4 +1,9 @@
+/// Enumeration of all possible events that the Shuvarie TUI might be handling.
+///
+/// An `Event` can be consumed by the `map_event()` method that maps the event to a message.
 pub enum Event {
+    /// Event from a terminal emulator via Termina
     Terminal(termina::Event),
+    /// Event from the Shuvarie core
     Core(shuvarie_core::Event),
 }
