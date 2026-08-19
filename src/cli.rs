88846af9a50eecb2df872e395f512a74b5aab4a8
Charles Dong <chardon_cs@proton.me>
2026-08-19 17:42:18 +0800

    style: update CLI help color

--- src/cli.rs
+++ src/cli.rs
@@ -1,6 +1,19 @@
-use clap::Parser;
+use clap::{
+    builder::{
+        styling::{AnsiColor, Style},
+        Styles,
+    },
+    Parser,
+};
+
+const STYLES: Styles = Styles::styled()
+    .header(AnsiColor::BrightGreen.on_default().bold())
+    .usage(Style::new().bold())
+    .literal(AnsiColor::Cyan.on_default())
+    .placeholder(AnsiColor::White.on_default());
 
 #[derive(Parser)]
+#[command(about, version, styles = STYLES)]
 pub struct Cli {
     /// Open the most recent session on startup instead of the home screen.
     #[arg(short, long)]
